6d74f751944a04d0b077b936dbaf1a6a381259de
crystalYY <972369976@qq.com>
2019-03-19 16:41:25 +0800

    修改登录界面背景及登录错误

--- js/login.js
+++ js/login.js
@@ -3,23 +3,27 @@ var userPassword=document.getElementById('userPassword');
 var btn=document.getElementById('btn');
 btn.onclick=function(e){
 	if(userName.value==''||userPassword.value==''){
+		console.log('heeee');
 		if(e&&e.preventDefault){
 			e.preventDefault();
 		}else{
 			window.event.returnValue=false;
 		}
+		return false;
 	}
 	if(userName.value==''){
 		document.getElementById('userNameError').innerHTML='用户名不能为空！';
 		userName.style.borderColor='#FF5A3D';
 		userName.style.borderWidth='2px';
+		return false;
 	}
 	if(userPassword.value==''){
 		document.getElementById('passwordError').innerHTML='密码不能为空！';
 		userPassword.style.borderColor='#FF5A3D';
 		userPassword.style.borderWidth='2px';
+		return false;
 	}
-	return false;
+	
 };
 userName.onfocus=function(){
 	document.getElementById('userNameError').innerHTML='';
